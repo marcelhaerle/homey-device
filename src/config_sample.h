ffa88ba8207f5448c9f675e6d0e06e9d5c12b0cd
Marcel Härle <marcel.haerle@sonnvest.de>
2021-10-10 15:08:39 +0200

    Connect to WiFi

---
+++ src/config_sample.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define MQTT_HOST ""
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "homey"
+#define HOMEY_DEVICE_ID "HDT-01"
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "config.h"
 
+void setup_wifi() {
+  Serial.print("* Connecting to WiFi ");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("* WiFi connected");
+  Serial.print("* IP: ");
+  Serial.print(WiFi.localIP());
+  Serial.println("");
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+
+  setup_wifi();
 }
 
 void loop() {
