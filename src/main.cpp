84327c999df8dd28249234ec3af52a29722e6f1f
Marcel Härle <marcel.haerle@sonnvest.de>
2021-10-10 15:47:37 +0200

    Send data via MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,22 @@
 #include <WiFi.h>
 #include "config.h"
 #include "h_sensors.h"
+#include <PubSubClient.h>
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(HOMEY_DEVICE_ID)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+    }
+  }
+}
 
 void setup_wifi() {
   Serial.print("* Connecting to WiFi ");
@@ -17,17 +33,43 @@ void setup_wifi() {
   Serial.println("");
 }
 
+void setup_mqtt() {
+   client.setServer(MQTT_HOST, MQTT_PORT);
+   reconnect();
+}
+
+void publish_data(char *json) {
+  if (!client.connected()) {
+    reconnect();
+  } else {
+    client.publish(MQTT_TOPIC, json);
+    Serial.println("MQTT: Published data");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(100);
 
   setup_sensors();
   setup_wifi();
+  setup_mqtt();
 }
 
 void loop() {
-  delay(1000);
+  delay(5000);
 
   sensorvalues sv = get_sensor_values();
-  Serial.printf("Temperature: %.2f\nHumidity: %.2f\n\n", sv.temperature, sv.humidity);
+
+  Serial.println("\n#########################");
+  Serial.printf("# Temperature: %.2f C\n", sv.temperature);
+  Serial.printf("# Humidity:    %.2f %%\n", sv.humidity);
+  Serial.println("#########################\n");
+
+  char *json = (char *)malloc(sizeof(char) * 200);
+  sprintf(json,
+    "{\"deviceId\": \"%s\",\"sensors\":[{\"type\": \"temperature\", \"unit\": \"Celsius\", \"value\": %.2f}, {\"type\": \"humidity\", \"unit\": \"%%\", \"value\": %.2f}]}",
+    HOMEY_DEVICE_ID, sv.temperature, sv.humidity);
+  publish_data(json);
+  free(json);
 }
